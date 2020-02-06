aa3e26dcd2a86a98f5cf340ff950cc8d4c3a798e
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-06 03:51:30 +0000

    fixed again size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  char charType;
-  int intType;
-  long int longintType;
-  long long int longlongintType;
-  float floatType;
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
 
-  printf("Size of a char: %u byte(s)\n", sizeof(charType));
-  printf("Size of an int: %u byte(s)\n", sizeof(intType));
-  printf("Size of a long int: %u byte(s)\n", sizeof(longintType));
-  printf("Size of a long long int: %u byte(s)\n", sizeof(longlongintType));
-  printf("Size of a float: %u byte(s)\n", sizeof(floatType));
-  return (0);
+printf("Size of a char: %u byte(s)\n", sizeof(charType));
+printf("Size of an int: %u byte(s)\n", sizeof(intType));
+printf("Size of a long int: %u byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %u byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %u byte(s)\n", sizeof(floatType));
+return (0);
 }
