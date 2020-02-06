77958977d4f11174b798982e03ac368337016c8c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-06 03:43:33 +0000

    fixed size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
   long long int longlongintType;
   float floatType;
 
-  printf("Size of a char: %ud byte(s)\n", sizeof(charType));
-  printf("Size of an int: %ud byte(n)\n", sizeof(intType));
-  printf("Size of a long int: %ud byte(s)\n", sizeof(longintType));
-  printf("Size of a long long int: %ud byte(s)\n", sizeof(longlongintType));
-  printf("Size of a float: %ud byte(s)\n", sizeof(floatType));
+  printf("Size of a char: %u byte(s)\n", sizeof(charType));
+  printf("Size of an int: %u byte(n)\n", sizeof(intType));
+  printf("Size of a long int: %u byte(s)\n", sizeof(longintType));
+  printf("Size of a long long int: %u byte(s)\n", sizeof(longlongintType));
+  printf("Size of a float: %u byte(s)\n", sizeof(floatType));
   return (0);
 }
