9aff6010130f923189f68ef329bdb802aeb181b6
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-06 23:28:48 +0000

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+/**
+ * main - main block
+ * Description: Print the alphabet in reverse order in lowercase.
+ */
+int main(void)
+{
+char alpha;
+
+for (alpha = 'z'; alpha >= 'a'; alpha--)
+{
+putchar(alpha);
+}
+putchar('\n');
+
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
