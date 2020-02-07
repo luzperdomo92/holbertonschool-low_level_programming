94005cdb76fbb887125dd9ce1338069af88f17f5
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-07 02:05:40 +0000

    fixed 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,22 +3,23 @@
  * main - main block
  * Description: all possible combinations of single-digit numbers.
  * Return: 0
- */
+*/
 int main(void)
 {
-  int a;
+int a;
 
-  for (a = 0; a <= 9; a++)
-    {
-      putchar(a + '0');
-      if (a != 9)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-
-  putchar('\n');
-
-  return (0);
+for (a = 0; a < 9; a++)
+{
+putchar(a + '0');
+if (a != 9)
+{
+putchar(',');
+putchar(' ');
 }
+}
+
+putchar('\n');
+
+return (0);
+}
+
