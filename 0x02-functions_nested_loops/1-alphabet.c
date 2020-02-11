f1cba5799e7a0c7260135117aecf96b1a009aa06
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-11 13:26:11 +0000

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_alphabet - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+void print_alphabet(void){
+char alp; 
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+_putchar(alp);
+}
+_putchar('\n');
+}
