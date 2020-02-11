4f97596e3a5cd1eddeb19dfa5cf099eab780ef61
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-11 04:36:57 +0000

    added 3-islower.c script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase.
+ *
+ * Return: Always void.
+ */
+int _islower(int c)
+{
+  if (c >= 'a' && c <= 'z')
+  {
+    return (1);
+  }
+  else
+  {
+    return (0);
+  }
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,7 +4,7 @@
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
-
+int _islower(int c);
 
 
 #endif
