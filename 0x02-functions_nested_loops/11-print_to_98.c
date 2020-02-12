7eaac00b8153977fc4b7be1229bf3b021f85d535
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-12 03:20:02 +0000

    added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 prints the numbers until 98
+ * @n: init type int
+ * Return: void
+ */
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (n = n; n >= 98; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (n = n; n <= 98; n++)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+
+printf("\n");
+}
