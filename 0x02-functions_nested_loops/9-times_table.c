37fb413b3f6fc091216c62f0d39d088500295306
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-12 03:02:29 +0000

    fixed 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,25 @@ void times_table(void)
 int multiplicand;
 int multiplier;
 int result;
+int tens;
 for (multiplicand = 0; multiplicand <= 9; multiplicand++)
 {
 for (multiplier = 0; multiplier <= 9; multiplier++)
 {
 result = multiplicand * multiplier;
-_putchar(result / 10 + '0');
+tens = result / 10;
+if (tens > 0)
+{
+_putchar(tens + '0');
+}
+else
+{
+_putchar(' ');
+}
 _putchar(result % 10 + '0');
 if (multiplier != 9)
 {
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 }
 }
