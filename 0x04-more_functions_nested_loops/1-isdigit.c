850322d7143f60ab3be889c19491cf9332326d0a
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-13 15:31:54 +0000

    added checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *int _isdigit - checks a character is numeric character (0-9) or not.
+ * @c: An ascii char to compare
+ * Return: 1 or 0.
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
