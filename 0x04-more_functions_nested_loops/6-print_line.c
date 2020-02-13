aa3f02fc781acf079eb696d81a2bbae7a92e271b
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-13 20:50:51 +0000

    added draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_line - prints line
+ *@n: times _ prints
+ * Return: Always void.
+ */
+void print_line(int n)
+{
+for ( ; n > 0; n--)
+{
+_putchar ('_');
+}
+_putchar ('\n');
+}
