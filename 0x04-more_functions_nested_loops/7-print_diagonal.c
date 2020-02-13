b4f9177bf74122d059a9352a2ee4076b33d1011f
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-13 21:24:08 +0000

    added diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints diagonals lines.
+ *@n: times _ prints
+ * Return: Always void.
+ */
+void print_diagonal(int n)
+{
+for ( ; n > 0; n--)
+{
+_putchar ('\\');
+}
+_putchar ('\n');
+}
+
+
+
+
+
+
+
+
