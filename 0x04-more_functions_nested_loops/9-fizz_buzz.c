1015940d879e222c3d76deac76a46102213b755b
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-14 03:35:53 +0000

    added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main- prints the Fizz-Buzz test
+* Return: Always 0.
+*/
+int main(void)
+{
+int count;
+for (count = 1; count <= 100; count++)
+{
+if (count % 3 == 0 && count % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (count % 3 == 0)
+{
+printf("Fizz");
+}
+else if (count % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", count);
+}
+if (count 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
+
