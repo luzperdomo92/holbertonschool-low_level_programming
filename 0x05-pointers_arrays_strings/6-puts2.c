3c0b9d4f8719dca25d152e2cd485d5fe610d3138
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-18 22:59:15 +0000

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * puts2 - prints every other character a string.
+ * puts2 -  prints a string.
  *@str:an array of int
  * Return: Always 0.
  */
 void puts2(char *str)
 {
 int a;
-for (a = 0; str[a] != '\0'; a += 2)
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
 {
 _putchar(str[a]);
+}
 }
 _putchar ('\n');
 }
