755a2b2d47962b92d2bf4daf2f7151a51137b48c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-19 02:24:54 +0000

    added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+/**
+* _strcpy -  duplicate  string.
+*@dest: destination string pointer
+*@src: source string pointer
+* Return: string pointer with duplication.
+*/
+char *_strcpy(char *dest, char *src)
+{
+int a;
+for (a = 0; src[a] != '\0'; a++)
+{
+dest[a] = src[a];
+}
+return (dest);
+}
+
