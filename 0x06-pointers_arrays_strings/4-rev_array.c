497bf71474532eb1102354b876b19b75d485bf3c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-21 04:27:26 +0000

    * reverse_array -  prints a string in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+* reverse_array -  prints a string in reverse
+*@a:an array of int
+*@n:size
+* Return: Always void.
+*/
+void reverse_array(int *a, int n)
+{
+int end;
+int i;
+int swap[n];
+
+end = n - 1;
+
+for (i = 0; i < n ; i++)
+{
+swap[end] = a[i];
+end--;
+}
+for (i = 0; i < n ; i++)
+{
+a[i] = swap[i];
+}
+}
+
