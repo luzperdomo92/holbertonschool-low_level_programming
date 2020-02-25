431afd558b2f95789fe7bcc05c9618ef79cca9fe
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-25 01:57:09 +0000

    fixed 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strchr - locates a character in a string.
- * @s: char variable
- * @c: char variable
- * Return: the value of pointer.
- */
+* _strchr - locates a character in a string.
+* @s: char variable
+* @c: char variable
+* Return: the value of pointer.
+*/
 char *_strchr(char *s, char c)
 {
 int count;
 char *pointer = NULL;
+
+if (s == NULL || c == NULL)
+{
+return (0);
+}
 for (count = 0; s[count] != '\0'; count++)
 {
 if (s[count] == c)
@@ -20,3 +25,4 @@ break;
 }
 return (pointer);
 }
+
