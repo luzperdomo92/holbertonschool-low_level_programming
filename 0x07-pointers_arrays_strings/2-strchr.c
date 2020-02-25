df96cd4eb844cc1bd21d31a1847c634fe1ff3cef
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-25 02:43:14 +0000

    fixed 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,8 @@
 char *_strchr(char *s, char c)
 {
 int count;
-char *pointer = NULL;
+char *pointer = 0;
 
-if (s == NULL || !c)
-{
-return (0);
-}
 for (count = 0; s[count] != '\0'; count++)
 {
 if (s[count] == c)
@@ -23,6 +19,12 @@ pointer = &s[count];
 break;
 }
 }
+
+if (s[count] == c)
+{
+  pointer = &s[count];
+}
+
 return (pointer);
 }
 
