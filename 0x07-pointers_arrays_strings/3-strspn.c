16215ba49244bdb7fad11ab9b3a1a717e2a87245
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-25 02:51:01 +0000

    fixed 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,19 +10,23 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int count_s;
 int count_accept;
 unsigned int max_bytes = 0;
-for (count_accept = 0; accept[count_accept] != '\0'; count_accept++)
-{
+
 for (count_s = 0; s[count_s] != '\0'; count_s++)
+{
+for (count_accept = 0; accept[count_accept] != '\0'; count_accept++)
 {
 if (s[count_s] == accept[count_accept])
 {
-if (count_s + 1 > max_bytes)
-{
-max_bytes = count_s + 1;
-}
+max_bytes++;
 break;
 }
 }
+if (s[count_s] != accept[count_accept])
+{
+break;
+}
+
 }
 return (max_bytes);
 }
+
