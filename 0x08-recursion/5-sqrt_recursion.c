deef66e4793f8e697da047c06b01ce6895219aa3
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-27 02:45:29 +0000

    added square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,30 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - prints the factorial of the numbers.
+ * real_sqrt_recursion - square root of the number.
  *@n: int variable
+ *@sq: int variable
  * Return:  0.
+ */
+int real_sqrt_recursion(int n, int sq)
+{
+if (sq * sq >= n)
+{
+return (sq);
+}
+else
+{
+return (real_sqrt_recursion(n, sq + 1));
+}
+}
+/**
+ * _sqrt_recursion - prints the square root.
+ *@n: int variable
+ * Return:  value.
  */
 int _sqrt_recursion(int n)
 {
-int sq = 1;
-while (sq * sq < n)
-{
-sq++;
-}
+int sq = 0;
+sq = real_sqrt_recursion(n, sq);
 if (n == sq *sq)
 {
 return (sq);
