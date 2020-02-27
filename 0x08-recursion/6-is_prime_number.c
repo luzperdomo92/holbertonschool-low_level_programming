16fd9c79caaf9f27af6eb1cd7039e2fcb9f4c166
Luz Perdomo <lilyperdomo92@gmail.com>
2020-02-27 03:52:13 +0000

    fixed prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,10 +3,9 @@
  * real_is_prime_number - find the prime number
  *@n: int variable
  *@div: int variable
- *@add: in variable.
  * Return: funcion prime number.
  */
-int real_is_prime_number(int n, int div, int add)
+int real_is_prime_number(int n, int div)
 {
 if (div == n)
 {
@@ -18,35 +17,27 @@ return (0);
 }
 else
 {
-return (real_is_prime_number(n, div + add, add));
+return (real_is_prime_number(n, div + 1));
 }
 }
-
 /**
- * is_prime_number - square root of the number.
+ * is_prime_number - find the prime number
  *@n: int variable
- * Return:  variable prime.
+ * Return: funcion prime number.
  */
 int is_prime_number(int n)
 {
 int prime;
 int div;
-int add;
 
-if (n == 0 || n == 1 || n == -1)
+if (n <= 0 || n == 1)
 {
 return (0);
 }
 if (n > 0)
 {
 div = 2;
-add = 1;
 }
-else
-{
-div = -2;
-add = -1;
-}
-prime = real_is_prime_number(n, div, add);
+prime = real_is_prime_number(n, div);
 return (prime);
 }
