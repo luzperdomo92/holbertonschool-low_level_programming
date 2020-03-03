a4957fa95bf1a5590d43596514cb20595be0ae8a
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-03 21:08:07 +0000

    added concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 	for (size = 0; s1[size] != '\0'; size++)
 	{
