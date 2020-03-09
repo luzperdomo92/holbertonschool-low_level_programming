f7818f6b72a1712d9827c21152c4f878431ce761
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-09 20:59:08 +0000

    added new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,16 +7,10 @@
  * @owner: owner of the dog
  * Return: On success 1.
  */
-typedef struct dog
+struct dog
 {
 	 char *name;
 	 float age;
 	 char *owner;
-} dog_t;
-
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
 #endif
