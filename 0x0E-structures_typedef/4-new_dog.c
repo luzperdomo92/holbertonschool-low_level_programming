22f76130f5a79ebc8249ab6d2c192619774c8134
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-10 03:23:27 +0000

    fixed creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,24 @@
 #include<stdlib.h>
 #include "dog.h"
 /**
+ * _strcpy -  duplicate  string.
+ *@dest: destination string pointer
+ *@src: source string pointer
+ * Return: string pointer with duplication.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -12,8 +30,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	new_dog->name = name;
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = malloc(sizeof(name));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	_strcpy(new_dog->name, name);
+
 	new_dog->age = age;
-	new_dog->owner = owner;
+
+	new_dog->owner = malloc(sizeof(owner));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	_strcpy(new_dog->owner, owner);
+
 	return (new_dog);
 }
