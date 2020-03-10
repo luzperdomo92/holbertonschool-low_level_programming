3e17cebf7ee19f1246e6adea0f562f5781ecc788
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-10 02:28:42 +0000

    added more changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: owner of the dog
  * Return: On success 1.
  */
-struct dog
+typedef  struct dog
 {
 	 char *name;
 	 float age;
 	 char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
