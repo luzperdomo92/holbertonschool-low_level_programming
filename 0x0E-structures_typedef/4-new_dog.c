ce5e64240d14a0227032a5407c5f54451da32d6e
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-10 03:34:29 +0000

    fixed creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,7 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name != NULL)
 	{
-		new_dog->name = malloc(sizeof(name));
+		new_dog->name = malloc(sizeof(name) + 1);
 		if (new_dog->name == NULL)
 		{
 			free(new_dog->name);
@@ -51,7 +51,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	if (owner != NULL)
 	{
-		new_dog->owner = malloc(sizeof(owner));
+		new_dog->owner = malloc(sizeof(owner) + 1);
 		if (new_dog->owner == NULL)
 		{
 			free(new_dog->name);
