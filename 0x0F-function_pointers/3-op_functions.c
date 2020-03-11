6bcd3ac8330a4eb7426763b7cde4851cd342c608
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-11 21:33:32 +0000

    added first files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add- adicction oparation
+ * @a: variable type int
+ * @b: variable type int
+ * Return: product of the operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- subtracction operation
+ * @a: variable type int
+ * @b: variable type int
+ * Return: product of the operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- mulpiplication operation
+ * @a: variable type int
+ * @b: variable type int
+ * Return: product of the operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * c);
+}
+
+/**
+ * op_div- division operation
+ * @a: variable type int
+ * @b: variable type int
+ * Return: product of the operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- module operation
+ * @a: variable type int
+ * @b: variable type int
+ * Return: product of the operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
