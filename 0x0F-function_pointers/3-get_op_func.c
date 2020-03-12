371edfebe7ad9357b6b3f84b58c5d745f8ef05fe
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-12 02:02:03 +0000

    fixed program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -22,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (sizeof(s) / 4 == 2 && *ops[i].op == *s && *(s + 1) == '\0')
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
