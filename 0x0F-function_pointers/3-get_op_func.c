3c7c3d96938f282e53c2629ecd3aee5f14494e9e
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-12 01:44:53 +0000

    fixed program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*ops[i].op == *s && *(s + 1) == '\0')
+		if (sizeof(s) / 4 == 2 && *ops[i].op == *s && *(s + 1) == '\0')
 		{
 			return (ops[i].f);
 		}
