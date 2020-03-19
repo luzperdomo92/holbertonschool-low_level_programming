aff451a215b26f6279e285c2b018b8107cd6a5f0
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-19 22:28:49 +0000

    added header and print all the elemnets

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list- prints all the elements of struct lists_t
+ * @h: is the pointer
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+in this proyect wue are goin to learn about
+When and why using linked lists vs arrays
+How to build and use linked lists
