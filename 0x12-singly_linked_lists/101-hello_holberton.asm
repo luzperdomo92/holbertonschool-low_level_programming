afb898237e3b2398f61542b6ed3592c97b0dd0aa
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-20 04:32:16 +0000

    update 64-bit program in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,16 @@
-section .data
+extern printf
+	    section .data
+msg:			    db 'Hello, Holberton', 0
+fmt:			    db "%s", 10, 0
 
-	    message db "Hello, Holberton", 10
-
-	section .text
-
-	global _start
-_start:
-	    mov rax, 1
-	    mov rdi, 1
-	    mov rsi, message
-	    mov rdx, 17
-	    syscall
-
-            mov rax, 60
-	    mov rdi, 0
-	    syscall
+	    section .text
+	    global main
+main:
+	    push rbp
+	    mov rdi,fmt
+	    mov rsi,msg
+	    mov rax,0
+	    call printf
+	    pop rbp
+	    mov rax,0
+	    ret
