59a962418f8fdb36a74a3be5bc22dc0ca7f8799c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-20 00:18:47 +0000

    fixed new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 /**
+ * str_len -  length of the string
+ * @string: string to be set.
+ * Return: the number of nodes.
+ */
+int str_len(const char *string)
+{
+	int count = 0;
+
+	while (string[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: start of the linked list
  * @str: string to be set as attribute of the next head.
@@ -10,11 +25,11 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (new == NULL || str == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = str_len(str);
 	new->next = *head;
 	*head = new;
 
