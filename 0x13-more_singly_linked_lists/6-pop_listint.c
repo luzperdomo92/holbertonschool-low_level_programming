d76ab8e93b0ee903dea601cbd93453fa2f7826ba
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-24 18:30:48 +0000

    added delete the head of the node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint -  deletes the head node
+ * @head: parameter to be set. 
+ * Return: the head node’s data (n).
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	tmp = *head;
+	*head = tmp->next;
+	n = tmp->n;
+
+	free(tmp);
+	return (n);
+}
