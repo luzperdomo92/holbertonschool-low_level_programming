a02964c008d040c9b0a46e11009c0e7441b839e6
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-24 14:21:53 +0000

    added readme, header and task 00 prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of struct listint_t
+ * @h: is the pointer
+ * Return: the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return(count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+in this proyect we are going to learn about
+How to use linked lists
+Start to look for the right source of information without too much help
