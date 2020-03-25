33ad820ef5e8ee8a7416dddc3beaf684db4e496f
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-25 04:25:24 +0000

    fixed 10-delete_nodeint.cc

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,30 +7,27 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
-	listint_t *node_before_index;
+	unsigned int count = -1;
 	listint_t *tmp_head = *head;
+	listint_t *node_after_index;
 
-	while (tmp_head != NULL)
-	{
-		if (count == index)
-		{
-			if (node_before_index != NULL)
-			{
-				node_before_index->next = tmp_head->next;
-			}
-			else
-			{
-				*head = tmp_head->next;
-			}
-			free(tmp_head);
-			return (1);
-		}
+	  while(tmp_head != NULL)
+	  {
+		  if(count + 1 == index)
+		  {
+			  node_after_index = tmp_head->next;
+			  free(tmp_head);
+			  tmp_head = node_after_index;
+			  if (count + 1 == 0)
+			  {
+				  *head = tmp_head;
+			  }
+			  return(1);
+		  }
 
-		node_before_index = tmp_head;
-		tmp_head = tmp_head->next;
-		count++;
-	}
+		  tmp_head = tmp_head->next;
+		  count++;
+	  }
 
-	return (-1);
+  return(-1);
 }
