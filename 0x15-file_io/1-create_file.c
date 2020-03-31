9620ae650071835235d7224fc3ce8e781892e1cd
Luz Perdomo <lilyperdomo92@gmail.com>
2020-03-31 23:04:57 +0000

    fixed function  that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - create a file
  * @filename: name of the file
- * @text_contentet: length of buffer
+ * @text_content: length of buffer
  * Return: actual number of letters it could read and print.
  */
 int create_file(const char *filename, char *text_content)
@@ -16,12 +16,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	file_desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if(file_desc == -1)
+	if (file_desc == -1)
 	{
 		return (-1);
 	}
 
-	if(text_content != NULL)
+	if (text_content == NULL)
 	{
 		return (1);
 	}
@@ -34,5 +34,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(file_desc);
-	return(1);
+	return (1);
 }
