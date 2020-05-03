c4fbc5b971fc23dc3f8b7a3a417944610de9b8af
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-03 16:22:37 -0500

    added 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+  
+#include "lists.h"
+/**
+ * free_dlistint - free the allocate memory
+ * @head: start of the linked list
+ * Return: no return, just free.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
