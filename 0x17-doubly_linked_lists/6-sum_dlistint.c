7862b7e74e3cbb3ea5664c11d36979c3c0903ac4
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-03 16:45:26 -0500

    added 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint -  sum of all the data (n).
+ * @head: parameter to be set.
+ * Return: the sum of all the data (n).
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
