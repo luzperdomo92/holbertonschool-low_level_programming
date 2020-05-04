ebcc98fe3cddf3e1c6a030bb96bd8dd91ae8bbf9
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-04 23:33:33 +0000

    update fixed

---
+++ 0x18-dynamic_libraries/strncpy.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strncpy -  copy array
+ *@dest:an array of char
+ *@src:an array of char
+ *@n:size
+ * Return: Always dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int concat;
+	int use_src = 1;
+	for (concat = 0; concat < n; concat++)
+	{
+		if (src[concat] == '\0')
+		{
+			use_src = 0;
+		}
+
+		if (use_src == 1)
+		{
+			dest[concat] = src[concat];
+		}
+		else
+		{
+			dest[concat] = '\0';
+		}
+	}
+	return (dest);
+}
