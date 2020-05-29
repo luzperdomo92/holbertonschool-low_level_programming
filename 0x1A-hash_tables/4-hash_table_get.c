3bdc0bb9dc84a822932eab7b05cd07c7875c2a86
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-29 21:46:56 +0000

    added task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ *  hash_table_get - retrieves a value associated with a key.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the index of.
+ *
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *value_search;
+
+	if (!ht || !key)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	value_search = ht->array[index];
+	while (value_search)
+	{
+		if (value_search->key == key)
+			return (value_search->value);
+		value_search = value_search->next;
+	}
+	return (NULL);
+}
+
