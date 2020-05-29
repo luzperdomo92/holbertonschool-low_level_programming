a520e98fee8b00f23237450a3215859dc4b4ba54
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-29 18:12:44 +0000

    added task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds or update an element to the hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the index of.
+ * @value: The value corresponding to a key
+ *
+ * Return:  1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (!new_node->key || !new_node->value)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	if (node != NULL)
+	{
+		while (node)
+		{
+			if (strcmp(key, node->key) == 0)
+			{
+				free(node->value);
+				node->value = strdup((char *)value);
+				return (1);
+			}
+			node = node->next;
+		}
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
