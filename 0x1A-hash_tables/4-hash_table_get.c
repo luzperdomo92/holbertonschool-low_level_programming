a60df1c67fafa5ee18e61b1bff8bae6fe3da8f3c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-29 23:12:13 +0000

    fixed logical syntaxis

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,19 +13,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *value_search;
 
-	if (ht == NULL || key == NULL)
+	if (!key || strcmp(key, "") == 0 || !ht)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
-
 	value_search = ht->array[index];
-	while (value_search)
+
+	for (; value_search != NULL; value_search = value_search->next)
 	{
-		if (value_search->key == key)
+		if (strcmp(value_search->key, key) == 0)
 			return (value_search->value);
-		value_search = value_search->next;
 	}
 	return (NULL);
 }
